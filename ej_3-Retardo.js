831bd4fbeb564f791742012b43520072b877ddd7
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-04-20 19:44:30 -0300

    Agrego console.log con título del ejercicio 3

--- ej_3-Retardo.js
+++ ej_3-Retardo.js
@@ -2,6 +2,7 @@
 // Ejercicio 3 :: Imprimir con Retraso
 // Usar una función flecha y setTimeout para imprimir cada elemento de un array con un retraso de 1 segundo por elemento.
 const arrayFrutas = ["manzana", "banana", "naranja", "pera", "uva", "kiwi"];
+console.log("-----  EJERCICIO 3  -----");
 console.log(`Imprimiendo, cada un segundo, los ${arrayFrutas.length} registros del array de frutas`);
 
 // Tuve que agregar el index del foreach porque sino me imprimía todas las frutas juntas despues de 1 segundo
